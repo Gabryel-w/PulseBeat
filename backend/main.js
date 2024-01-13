3f014ee82a9f41edce002826be50d5ee5191312a
Joao-m1guel <joaomiguel@gmail.com>
2024-01-13 11:24:03 -0300

    Server First commit

--- backend/main.js
+++ backend/main.js
@@ -3,13 +3,12 @@ const path = require("path");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
-
+const PATH = path.resolve(__dirname)
 
 app.use(express.static(path.resolve(__dirname, '../frontend')));
 
 app.get('/', (req, res) =>{
-    console.log(`${req.baseUrl}/${req.url}`);
-    res.send("WELCOME!");
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
 })
 
 
