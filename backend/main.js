fa816e6acdc6c7dfbe8aecc70bf828b1d05b4ef2
Joao-m1guel <joaomiguel@gmail.com>
2024-01-13 19:51:55 -0300

    Commented server file

--- backend/main.js
+++ backend/main.js
@@ -1,3 +1,4 @@
+// CONST DECLARATIONS
 const express = require("express");
 const path = require("path");
 
@@ -5,14 +6,22 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const PATH = path.resolve(__dirname)
 
+
+// Serving static files
 app.use(express.static(path.resolve(__dirname, '../frontend')));
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
+
+// Routes
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, '../frontend/build', "index.html"));
 })
+app.post('/login', (req, res) =>{
+    res.sendStatus(200)
+})
 
 
+// Listen to the determined port
 app.listen(PORT, () => {
     console.log(`server on http://localhost:${PORT}`);
 })
